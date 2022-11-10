5effa44411f2302c336a9cc4b5e8b3147d8fcb34
ruthiep5 <82891285+ruthiep5@users.noreply.github.com>
2022-11-10 14:41:49 -0600

    test

--- drainingCalibration.py
+++ drainingCalibration.py
@@ -4,43 +4,10 @@ from ezGraph import *
 from rStats import *
 
 #Model
-# h = 2t - 3.22
-
-def avg(lst):
-    return sum(lst)/len(lst)
-
 
 #Parameters
 dt = 1
 nsteps = 160
-
-
-# Model:
-# - Representation of the real world.
-# -- Requires some data about the real world.
-# - Analytical model: Equation or function that represents (best fits) the data. 
-# -- Linear, polynomial, exponential, sinusoidal, logarithmic, hyperbolic, radical. 
-# -- Plug in an input and the equation gives the output. 
-# --- h(t) = 2t + 7
-# ---- input: t
-# ---- output: h
-# -- Only works for well behaved systems (simple systems). Not a whole lot of interactions that will introduce errors. Non-chaotic.
-# -- often use calculus to find the equations
-# - PHYSICAL MODELS - 
-# -- Small scale physical thing (you can touch it).
-# -- Can be more complex
-# -- Problems with scaling
-# - NUMERICAL MODELS
-# -- Usually computer models because there's a lot of calculations (algebraic).
-# -- Usually break the system into smaller parts that interact with each other.
-# -- The computer is used to keep track of all the interactions.
-# -- We'll focus on things that change over time.  
-# -- We'll focus on finite difference models. 
-
-
-# linear model
-# m = 5.03
-# b = -17.2
 
 r = 2.25 #radius (cm)
 Q = 0 # Volume inflow rate: (dv/dt) (cubic cm / s)
@@ -67,9 +34,11 @@ for t in range(nsteps):
   
     modelTime = t * dt
 
+#filling
     dh = Q * dt / (np.pi * r **2)
     h = h + dh
 
+#draining
     dVdt = -k * h 
     dh = dVdt * dt / (np.pi * r**2)
     h = h + dh
@@ -88,12 +57,6 @@ for t in range(nsteps):
 #     Ave = s / s.len
 #     print(s)
 
-print('time:', x_measured)   
-print('h_measured:', y_measured)
-print("h_modeled:", y_modeled)
-
-print(f'xavg measured =  {avg(x_measured)}')
-print(f'yavg measured =  {avg(y_measured)}')
 # calculate average values
 
 def absoluteError(lst1, lst2):
@@ -116,8 +79,10 @@ def res(lst1, lst2):
     return s
 
 r = res(y_measured, y_modeled)
-print(f'residual = {r}')
 
+
+def avg(lst):
+    return sum(lst)/len(lst)
 
 def dsq(lst1):
     n = len(lst1)
@@ -129,15 +94,22 @@ def dsq(lst1):
     return s
 
 d = dsq(y_measured)
-print(f'difference = {d}')
 
 def rSquared(lst1, lst2):
     r2 = 1 - (res(lst1, lst2) / dsq(lst1) )
     return r2
 
 r2 = rSquared(y_measured, y_modeled)
-print(f'r squared = {r2}')
 
+
+print('time:', x_measured)   
+print('h_measured:', y_measured)
+print("h_modeled:", y_modeled)
+print(f'xavg measured =  {avg(x_measured)}')
+print(f'yavg measured =  {avg(y_measured)}')
+print(f'residual = {r}')
+print(f'difference = {d}')
+print(f'r squared = {r2}')
 
 #draw graph
 graph.keepOpen()
--- rStats.py
+++ rStats.py
@@ -8,12 +8,26 @@ def findAvg(lst):
     avg = t/n
     return avg
 
-def resSq(lst1, lst2):
-    #finds sum of residuals squared
-    t = 0
-    for i in range(len(lst1)):
-        t += (lst1[i]-lst2[i])**2
-    return t
+def absoluteError(lst1, lst2):
+    n = len(lst1)
+    a = 0
+    for i in range(n):
+        # print(i,lst1[i], lst2[i])
+        d = lst1[i] - lst2[i]
+        a += abs(d)
+    return(a)
+    
+def avg(lst):
+    return sum(lst)/len(lst)
+
+def res(lst1, lst2):
+    n = len(lst1)
+    s = 0
+    for i in range(n):
+       # print(i,lst1[i], lst2[i])
+        d = (lst1[i] - lst2[i]) ** 2
+        s += d
+    return s
 
 def meanDif(lst):
     t = 0
@@ -21,4 +35,16 @@ def meanDif(lst):
     for x in lst:
         t += (x - avg)**2
     return t
-        
+
+def dsq(lst1):
+    n = len(lst1)
+    s = 0
+    for i in range(n):
+       # print(i,lst1[i], lst2[i])
+        d = (lst1[i] - avg(lst1)) ** 2
+        s += d
+    return s
+
+def rSquared(lst1, lst2):
+    r2 = 1 - (res(lst1, lst2) / dsq(lst1) )
+    return r2
